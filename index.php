1edecae9dfdded8896d6b2d101fa95b632ecdde7
Sacha Schmid <schmid.sacha@gmail.com>
2015-05-28 21:57:29 +0200

    Parse data seperatly from displaying it (closes #2)
    Visit /parse.php to parse the data. This dramatically improves load
    time for the actual search page.

--- index.php
+++ index.php
@@ -1,123 +1,8 @@
 <?php
 
 require_once 'config/config.php';
+$files = file_get_contents('data.json', true);
 
-// https://blog.mayflower.de/561-Import-and-export-data-using-PHPExcel.html
-
-require_once 'vendor/autoload.php';
-
-$parser = new \Smalot\PdfParser\Parser();
-
-$files = [];
-
-$path = realpath(SVN_PATH);
-$objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
-
-$excludes = [
-    '7 Hauptordner SC/',
-    '.svn/',
-    '.js',
-    '.css',
-    '.svg',
-    '.php',
-    '.gif',
-    '.jpg',
-    '~'
-];
-
-foreach ($objects as $name => $object){
-    $excludeItem = false;
-
-    foreach ($excludes as $exclude) {
-        if (strpos($name, $exclude) != false) {
-            $excludeItem = true;
-        }
-    }
-
-    if ($excludeItem) {
-        continue;
-    }
-
-    $content = "";
-
-    if (strpos($name, ".docx")) {
-        $content = read_docx($name);
-    } elseif (strpos($name, ".doc")) {
-        $content = read_doc($name);
-    } elseif (strpos($name, ".xlsx")) {
-        $objPHPExcel = PHPExcel_IOFactory::load($name);
-        foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
-            $worksheetTitle     = $worksheet->getTitle();
-            $highestRow         = $worksheet->getHighestRow(); // e.g. 10
-            $highestColumn      = $worksheet->getHighestColumn(); // e.g 'F'
-            $highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);
-            $nrColumns = ord($highestColumn) - 64;
-            $content .= "<br>The worksheet ".$worksheetTitle." has ";
-            $content .= $nrColumns . ' columns (A-' . $highestColumn . ') ';
-            $content .= ' and ' . $highestRow . ' row.';
-            $content .= '<br>Data: <table border="1"><tr>';
-            for ($row = 1; $row <= $highestRow; ++ $row) {
-                $content .= '<tr>';
-                for ($col = 0; $col < $highestColumnIndex; ++ $col) {
-                    $cell = $worksheet->getCellByColumnAndRow($col, $row);
-                    $val = $cell->getValue();
-                    $dataType = PHPExcel_Cell_DataType::dataTypeForValue($val);
-                    $content .= '<td>' . $val . '</td>';
-                }
-                $content .= '</tr>';
-            }
-            $content .= '</table>';
-            $content = strip_tags($content);
-        }
-    } elseif (strpos($name, ".pdf")) {
-        // $pdf = $parser->parseFile($name);
-        //$content = $pdf->getText();
-    }
-
-    array_push($files, array("filename" => pathinfo($name, PATHINFO_FILENAME), "extension" => pathinfo($name, PATHINFO_EXTENSION), "path" => $name/* , "content" => $content */));
-}
-
-function read_doc($filename) {
-    $fileHandle = fopen($filename, "r");
-    $line = @fread($fileHandle, filesize($filename));
-    $lines = explode(chr(0x0D),$line);
-    $outtext = "";
-    foreach($lines as $thisline)
-    {
-        $pos = strpos($thisline, chr(0x00));
-        if (($pos !== FALSE)||(strlen($thisline)==0))
-        {
-        } else {
-            $outtext .= $thisline." ";
-        }
-    }
-    $outtext = preg_replace("/[^a-zA-Z0-9\s\,\.\-\n\r\t@\/\_\(\)]/","",$outtext);
-    return $outtext;
-}
-
-function read_docx($filename){
-
-    $content = '';
-
-    $zip = zip_open($filename);
-
-    if (!$zip || is_numeric($zip)) return false;
-
-    while ($zip_entry = zip_read($zip)) {
-        if (zip_entry_open($zip, $zip_entry) == FALSE) continue;
-        if (zip_entry_name($zip_entry) != "word/document.xml") continue;
-        $content .= zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
-        zip_entry_close($zip_entry);
-    }// end while
-
-    zip_close($zip);
-
-    $content = str_replace('</w:r></w:p></w:tc><w:tc>', " ", $content);
-    $content = str_replace('</w:r></w:p>', "\r\n", $content);
-    $striped_content = strip_tags($content);
-
-    return $striped_content;
-}
 ?>
 
 <html ng-app="myApp" ng-controller="FileViewer">
@@ -127,11 +12,11 @@ function read_docx($filename){
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <script type="text/javascript">
         angular.module('myApp', [])
-            .controller('FileViewer', function($scope, $sce) {
+            .controller('FileViewer', function ($scope, $sce) {
                 $scope.documentRoot = "<?php echo realpath(SVN_PATH); ?>";
-                $scope.files = <?php echo json_encode($files) ?>;
+                $scope.files = <?php echo $files ?>;
                 $scope.data = {
-                    search: ""
+                    search: ''
                 };
 
                 $scope.options = {
@@ -139,6 +24,7 @@ function read_docx($filename){
                     showPreview: true,
                     showPath: true
                 };
+
                 $scope.extensionFilter = function(file) {
                     return !$scope.options.includeAll
                         ? file.extension === 'docx'
@@ -150,16 +36,32 @@ function read_docx($filename){
                     || file.extension === 'pdf'
                     || file.extension === 'txt'
                         : true;
-                };
-                $scope.getIcon = function(extension) {
-                    var file = '';
-                    if (extension === 'xlsx' || extension === 'xls') file = 'fa-file-excel-o excelColor';
-                    if (extension === 'docx' || extension === 'doc') file = 'fa-file-word-o wordColor';
-                    if (extension === 'pptx' || extension === 'ppt') file = 'fa-file-powerpoint-o powerpointColor';
-                    if (extension === 'pdf') file = 'fa-file-pdf-o pdfColor';
-                    if (extension === 'txt') file = 'fa-file-text-o txtColor';
-                    return file;
-                };
+                }
+
+                $scope.getIcon = function (extension) {
+                    var file = ''
+
+                    switch (extension) {
+                        case 'xlsx': case 'xls':
+                            file = 'fa-file-excel-o excelColor'
+                            break
+                        case 'docx': case 'doc':
+                            file = 'fa-file-word-o wordColor'
+                            break
+                        case 'pptx': case 'ppt':
+                            file = 'fa-file-powerpoint-o powerpointColor'
+                            break
+                        case 'pdf':
+                            file = 'fa-file-pdf-o pdfColor'
+                            break
+                        case 'txt':
+                            file = 'fa-file-text-o txtColor'
+                            break
+                    }
+
+                    return file
+                }
+
                 $scope.displayContent = function(file) {
                     if ($scope.data.search.length > 0 && $scope.options.showPreview) {
                         var area = 20;
@@ -174,6 +76,7 @@ function read_docx($filename){
                         return $sce.trustAsHtml(output);
                     }
                 };
+
                 $scope.findOccurances = function(file) {
                     var string = file.content.toLowerCase();
                     var subString = $scope.data.search.toLowerCase();
@@ -189,17 +92,21 @@ function read_docx($filename){
                     }
                     return(n);
                 };
+
                 function nl2br (str) {
                     return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1<br />$2');
                 }
+
                 function preg_quote(str, delimiter) {
                     return String(str)
                         .replace(new RegExp('[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\' + (delimiter || '') + '-]', 'g'), '\\$&');
                 }
+
                 function highlight(data, search)
                 {
                     return data.replace( new RegExp( "(" + preg_quote( search ) + ")" , 'gi' ), "<strong style='color:#FF0000'>$1</strong>" );
                 }
+
                 $scope.getPath = function(file) {
                     return file.path.slice($scope.documentRoot.length, -(file.filename.length + file.extension.length + 1));
                 }
---
+++ parse.php
@@ -0,0 +1,127 @@
+<?php
+
+require_once 'config/config.php';
+
+// https://blog.mayflower.de/561-Import-and-export-data-using-PHPExcel.html
+
+require_once 'vendor/autoload.php';
+
+$parser = new \Smalot\PdfParser\Parser();
+
+$files = [];
+
+$path = realpath(SVN_PATH);
+$objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+
+$excludes = [
+    '7 Hauptordner SC/',
+    '.svn/',
+    '.js',
+    '.css',
+    '.svg',
+    '.php',
+    '.gif',
+    '.jpg',
+    '~'
+];
+
+foreach ($objects as $name => $object){
+    $excludeItem = false;
+
+    foreach ($excludes as $exclude) {
+        if (strpos($name, $exclude) != false) {
+            $excludeItem = true;
+        }
+    }
+
+    if ($excludeItem) {
+        continue;
+    }
+
+    $content = "";
+
+    if (strpos($name, ".docx")) {
+        $content = read_docx($name);
+    } elseif (strpos($name, ".doc")) {
+        $content = read_doc($name);
+    } elseif (strpos($name, ".xlsx")) {
+        $objPHPExcel = PHPExcel_IOFactory::load($name);
+        foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
+            $worksheetTitle     = $worksheet->getTitle();
+            $highestRow         = $worksheet->getHighestRow(); // e.g. 10
+            $highestColumn      = $worksheet->getHighestColumn(); // e.g 'F'
+            $highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);
+            $nrColumns = ord($highestColumn) - 64;
+            $content .= "<br>The worksheet ".$worksheetTitle." has ";
+            $content .= $nrColumns . ' columns (A-' . $highestColumn . ') ';
+            $content .= ' and ' . $highestRow . ' row.';
+            $content .= '<br>Data: <table border="1"><tr>';
+            for ($row = 1; $row <= $highestRow; ++ $row) {
+                $content .= '<tr>';
+                for ($col = 0; $col < $highestColumnIndex; ++ $col) {
+                    $cell = $worksheet->getCellByColumnAndRow($col, $row);
+                    $val = $cell->getValue();
+                    $dataType = PHPExcel_Cell_DataType::dataTypeForValue($val);
+                    $content .= '<td>' . $val . '</td>';
+                }
+                $content .= '</tr>';
+            }
+            $content .= '</table>';
+            $content = strip_tags($content);
+        }
+    } elseif (strpos($name, ".pdf")) {
+        // $pdf = $parser->parseFile($name);
+        //$content = $pdf->getText();
+    }
+
+    array_push($files, array("filename" => pathinfo($name, PATHINFO_FILENAME), "extension" => pathinfo($name, PATHINFO_EXTENSION), "path" => $name/* , "content" => $content */));
+}
+
+function read_doc($filename) {
+    $fileHandle = fopen($filename, "r");
+    $line = @fread($fileHandle, filesize($filename));
+    $lines = explode(chr(0x0D),$line);
+    $outtext = "";
+    foreach($lines as $thisline)
+    {
+        $pos = strpos($thisline, chr(0x00));
+        if (($pos !== FALSE)||(strlen($thisline)==0))
+        {
+        } else {
+            $outtext .= $thisline." ";
+        }
+    }
+    $outtext = preg_replace("/[^a-zA-Z0-9\s\,\.\-\n\r\t@\/\_\(\)]/","",$outtext);
+    return $outtext;
+}
+
+function read_docx($filename){
+
+    $content = '';
+
+    $zip = zip_open($filename);
+
+    if (!$zip || is_numeric($zip)) return false;
+
+    while ($zip_entry = zip_read($zip)) {
+        if (zip_entry_open($zip, $zip_entry) == FALSE) continue;
+        if (zip_entry_name($zip_entry) != "word/document.xml") continue;
+        $content .= zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+        zip_entry_close($zip_entry);
+    }// end while
+
+    zip_close($zip);
+
+    $content = str_replace('</w:r></w:p></w:tc><w:tc>', " ", $content);
+    $content = str_replace('</w:r></w:p>', "\r\n", $content);
+    $striped_content = strip_tags($content);
+
+    return $striped_content;
+}
+
+$dataFile = fopen('data.json', 'w');
+fwrite($dataFile, json_encode($files));
+fclose($dataFile);
+
+header('Location: /');
+die();
