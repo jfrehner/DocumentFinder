5413eec5c20d5b64141669081e65eca166b45afe
Andreas Gassmann <andreas@andreasgassmann.ch>
2015-11-30 16:20:27 +0100

    Read directory with node.js and remove PHP

--- main.js
+++ main.js
@@ -1,11 +1,71 @@
+'use strict'
+
 const electron = require('electron')
+const ipc = require('electron').ipcMain
+const dialog = require("dialog")
+const recursive = require('recursive-readdir')
+const fs = require('fs')
 
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const Shell = electron.shell
 const Menu = electron.Menu
 
-var mainWindow = null
+let mainWindow = null
+let folder = null;
+
+ipc.on('selectFolder', function(event, args) {
+  console.log('selectFolder')
+
+  folder = dialog.showOpenDialog({
+    properties: [ 'openDirectory' ]
+  });
+
+  function ignoreFunc(file, stats) {
+    // `file` is the absolute path to the file, and `stats` is an `fs.Stats`
+    // object returned from `fs.lstat()`.
+    let allowed = [
+      'docx',
+      'doc',
+      'xlsx',
+      'xls',
+      'pptx',
+      'ppt',
+      'pdf',
+      'txt']
+
+    let isValidFile = false
+
+    if (file) {
+      isValidFile = allowed.filter((a) => {
+        return file.indexOf(a) !== -1
+      }).length > 0;
+    }
+    //return !stats.isDirectory() || !(file && path.basename(file).lastIndexOf('.') !== -1 && allowed.map((str) =>{return str === path.basename(file).substr(path.basename(file).lastIndexOf('.'))}));
+    return !(stats.isDirectory() && file.indexOf('.svn') === -1 || isValidFile);
+  }
+
+// Ignore files named 'foo.cs' and descendants of directories named test
+  recursive(folder[0], [ignoreFunc], function (err, files) {
+    // Files is an array of filename
+
+    let myFiles = [];
+    files.forEach((f) => myFiles.push({
+      "filename": f.substr(f.lastIndexOf('/') + 1),
+      "extension": f.substr(f.lastIndexOf('.') + 1),
+      "path": f,
+      "content": ""
+    }));
+
+    fs.writeFile('data.js', JSON.stringify(myFiles), () => {
+      console.log('wrote file!')
+    })
+  })
+
+  if (folder) {
+    console.log(folder)
+  }
+})
 
 app.on('window-all-closed', () => app.quit())
 
@@ -16,6 +76,8 @@ app.on('ready', () => {
       center: true
   })
   mainWindow.loadURL('file://' + __dirname + '/index.html')
+
+  mainWindow.openDevTools();
 
   mainWindow.on('closed', () => mainWindow = null)
 
@@ -128,6 +190,6 @@ app.on('ready', () => {
     }
   ]
 
-  menu = Menu.buildFromTemplate(template)
+  let menu = Menu.buildFromTemplate(template)
   Menu.setApplicationMenu(menu)
 })
